ef8b5e6918048806c69e25080c12fca8d4e85986
bacon0817 <bacon0817.ix@gmail.com>
2024-06-24 15:33:13 +0900

    漢字修正

--- pyTUImenu/pyTUImenu/pyTUImenu.py
+++ pyTUImenu/pyTUImenu/pyTUImenu.py
@@ -117,7 +117,7 @@ class pyTUImenu():
                         return 1
                     #終了する
                     if mode==exit_func_index and not exit_func_index==0:
-                        is_end=Confirm.ask(f'アプリケーションをしゅうりょうしますか？')
+                        is_end=Confirm.ask(f'アプリケーションを終了しますか？')
                         if is_end:
                             return
                     #関数が指定されていない場合
